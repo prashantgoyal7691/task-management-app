296041f924aa06979c6f778a90c0b237b6f3409d
prashant <prashantgo7691@gmail.com>
2026-02-22 17:37:20 +0530

    render key changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "task-management-app-kappa-lilac.vercel.app",
+  baseURL: "https://task-management-app-sxae.onrender.com",
 });
 
 // Automatically attach token to every request
